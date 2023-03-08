29b219714bbe61e15f252bcdfcc2daafe78565bf
Yahya Darman <102567151+yahya-darman@users.noreply.github.com>
2023-03-08 09:16:30 -0500

    Update Hello_World.c

--- Hello_World.c
+++ Hello_World.c
@@ -4,9 +4,3 @@ int main()
 {
     printf("Hello World\n");
 }
-
-// Compile, then run `./a.out 1>stdout.txt 2>stderr.txt`
-// Then run `echo $?`
-// stdout.txt should contain "hello, world"
-// stderr.txt should be empty
-// Your `echo $?` command should return 0
